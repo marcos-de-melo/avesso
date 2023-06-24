1a86e061db986ff437b8f614a42d5080ea86668a
Marcos de Melo <marcdmelo@gmail.com>
2023-06-24 15:31:11 -0300

    Primeiro Comit

---
+++ chat.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EduFofoca Chat</title>
+</head>
+
+<body>
+    <header class="main-header container">
+        <div class="content">
+            <img width="40" src="./img/logo.png" alt="">
+        </div>
+    </header>
+    <main>
+
+        <section class="container">
+            <div class="box-lista-msg content">
+                <article class="msg-box msg-you">
+                    <img class="logo-avatar" width="50" src="https://github.com/marcos-de-melo.png" alt="Avatar">
+                    <link rel="stylesheet" href="./css/style.css">
+                    <div>
+                        <h2>Marcos</h2>
+                        <p>Minha Mensagem aqui,... sit ing elit. Totam dicta, adipisci nisi odio porro aliquid voluptatem!</p>
+                        <p>13:41</p>
+                    </div>
+                </article>
+                <article class="msg-box msg-others">
+                    <img class="logo-avatar" width="50" src="https://github.com/marcos-de-melo.png" alt="Avatar">
+                    <div>
+                        <h2>Marcos</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum molestias doloremque suscipit error, pariatur maiores deleniti quod nulla tempore? Dolorum rem quo ad inventore. Debitis velit voluptates consectetur vitae laudantium!</p>
+                        <p class="msg-time">13:41</p>
+                    </div>
+
+
+                </article>
+            </div>
+        </section>
+
+    </main>
+    <footer>
+        <form action="" method="post">
+            <input type="text" name="txtMsg"> <button type="submit">Enviar</button>
+        </form>
+        <a href="http://">Atualizar</a>
+    </footer>
+</body>
+
+</html>
